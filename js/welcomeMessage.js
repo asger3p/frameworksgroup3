b1e81d58c5af75d2791f8a9684f3e9b09420abed
Asger P <aspj@itu.dk>
2025-03-10 15:31:32 +0100

    refactor welcome message functionality and update home page

--- js/welcomeMessage.js
+++ js/welcomeMessage.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const userName = localStorage.getItem("fname");
-    const welcomeMessage = document.getElementById("welcomeMessage");
-    const loginNavItem = document.getElementById("loginNavItem");
-    const signupNavItem = document.getElementById("signupNavItem");
-  
-    if (userName) {
-      welcomeMessage.textContent = `Welcome, ${userName}`;
-      loginNavItem.style.display = "none";
-      signupNavItem.style.display = "none";
-    } else {
-      welcomeMessage.style.display = "none";
-    }
-  });
+  const userName = localStorage.getItem("fname");
+  const welcomeMessage = document.getElementById("welcomeMessage");
+
+  if (userName) {
+    welcomeMessage.textContent = `Welcome, ${userName}!`;
+  } else {
+    welcomeMessage.textContent = "Welcome!";
+  }
+});
