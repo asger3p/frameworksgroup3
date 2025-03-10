7f8d4c28ffed7472eb542dc81e15cae4c4734723
Asger P <aspj@itu.dk>
2025-03-10 15:15:15 +0100

    refactored welcome message functionality to seperate js file

---
+++ js/welcomeMessage.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const userName = localStorage.getItem("fname");
+    const welcomeMessage = document.getElementById("welcomeMessage");
+    const loginNavItem = document.getElementById("loginNavItem");
+    const signupNavItem = document.getElementById("signupNavItem");
+  
+    if (userName) {
+      welcomeMessage.textContent = `Welcome, ${userName}`;
+      loginNavItem.style.display = "none";
+      signupNavItem.style.display = "none";
+    } else {
+      welcomeMessage.style.display = "none";
+    }
+  });
