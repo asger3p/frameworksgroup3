75f0527bc5ed038d7e3a547331a970e02bd0f6cc
Asger P <aspj@itu.dk>
2025-03-10 17:48:24 +0100

    refactor carousels.js to use dynamic product data and update card structure in home.html

--- js/carousels.js
+++ js/carousels.js
@@ -2,225 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const carousel = [
     {
       id: "featuredCarousel",
-      cards: [
-        {
-          title: "Cumin",
-          url: "/pages/product_description_generel.html?productId=product_cuminSeeds",
-          text: "This is the content for card 1",
-          image: "/images/1-cumin_seeds.jpg",
-        },
-        {
-          title: "Coriander",
-          url: "/pages/product_description_generel.html?productId=product_corianderSeeds",
-          text: "This is the content for card 2",
-          image: "/images/2-coriander_seeds.jpg",
-        },
-        {
-          title: "Cardamom",
-          url: "/pages/product_description_generel.html?productId=product_cardamomPods",
-          text: "This is the content for card 3",
-          image: "/images/3-cardamom_pods.jpg",
-        },
-        {
-          title: "Star Anise",
-          url: "/pages/product_description_generel.html?productId=product_starAnise",
-          text: "This is the content for card 4",
-          image: "/images/4-star_anise.jpg",
-        },
-        {
-          title: "Cloves",
-          url: "/pages/product_description_generel.html?productId=product_cloves",
-          text: "This is the content for card 5",
-          image: "/images/5-cloves.jpg",
-        },
-      ],
+      cards: products.filter((product) =>
+        ["product_cuminSeeds"].includes(product.id)
+      ),
     },
     {
       id: "allCarousel",
-      cards: [
-        {
-          title: "Cumin",
-          text: "This is the content for card 1",
-          image: "/images/1-cumin_seeds.jpg",
-        },
-        {
-          title: "Coriander",
-          text: "This is the content for card 2",
-          image: "/images/2-coriander_seeds.jpg",
-        },
-        {
-          title: "Cardamom",
-          text: "This is the content for card 3",
-          image: "/images/3-cardamom_pods.jpg",
-        },
-        {
-          title: "Star Anise",
-          text: "This is the content for card 4",
-          image: "/images/4-star_anise.jpg",
-        },
-        {
-          title: "Cloves",
-          text: "This is the content for card 5",
-          image: "/images/5-cloves.jpg",
-        },
-        {
-          title: "Bay Leaves",
-          text: "This is the content for card 1",
-          image: "/images/6-bay_leaves.jpg",
-        },
-        {
-          title: "Turmeric",
-          text: "This is the content for card 2",
-          image: "/images/7-turmeric.jpg",
-        },
-        {
-          title: "Paprika",
-          text: "This is the content for card 3",
-          image: "/images/8-paprika.jpg",
-        },
-        {
-          title: "Cinanmon",
-          text: "This is the content for card 4",
-          image: "/images/9-cinnamon.jpg",
-        },
-        {
-          title: "Cloves",
-          text: "This is the content for card 5",
-          image: "/images/10-ginger.jpg",
-        },
-        {
-          title: "Chili",
-          text: "This is the content for card 1",
-          image: "/images/11-chili.jpg",
-        },
-        {
-          title: "Oregano",
-          text: "This is the content for card 2",
-          image: "/images/12-oregano.jpg",
-        },
-        {
-          title: "Garam Masala",
-          text: "This is the content for card 3",
-          image: "/images/13-garammasala.jpg",
-        },
-        {
-          title: "Curry",
-          text: "This is the content for card 4",
-          image: "/images/14-curry.jpg",
-        },
-        {
-          title: "Taco",
-          text: "This is the content for card 5",
-          image: "/images/15-taco.jpg",
-        },
-        {
-          title: "Zaatar",
-          text: "This is the content for card 5",
-          image: "/images/16-zaatar.jpg",
-        },
-        {
-          title: "Chinese Five Spice",
-          text: "This is the content for card 5",
-          image: "/images/17-chinese_five_spice.jpg",
-        },
-        {
-          title: "Ras El Hanout",
-          text: "This is the content for card 5",
-          image: "/images/18-ras_el_hanout.jpg",
-        },
-      ],
+      cards: products,
     },
     {
       id: "mediterraneanCarousel",
-      cards: [
-        {
-          title: "Card 1",
-          text: "This is the content for card 1",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 2",
-          text: "This is the content for card 2",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 3",
-          text: "This is the content for card 3",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 4",
-          text: "This is the content for card 4",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 5",
-          text: "This is the content for card 5",
-          image: "/images/no_image.jpg",
-        },
-      ],
+      cards: products.filter((p) => p.cuisine === "Mediterranean"),
     },
     {
       id: "indianCarousel",
-      cards: [
-        {
-          title: "Card 1",
-          text: "This is the content for card 1",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 2",
-          text: "This is the content for card 2",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 3",
-          text: "This is the content for card 3",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 4",
-          text: "This is the content for card 4",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 5",
-          text: "This is the content for card 5",
-          image: "/images/no_image.jpg",
-        },
-      ],
+      cards: products.filter((p) => p.cuisine === "Indian"),
     },
     {
       id: "asianCarousel",
-      cards: [
-        {
-          title: "Card 1",
-          text: "This is the content for card 1",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 2",
-          text: "This is the content for card 2",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 3",
-          text: "This is the content for card 3",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 4",
-          text: "This is the content for card 4",
-          image: "/images/no_image.jpg",
-        },
-        {
-          title: "Card 5",
-          text: "This is the content for card 5",
-          image: "/images/no_image.jpg",
-        },
-      ],
+      cards: products.filter((p) => p.cuisine=== "Asian"),
     },
-  ];  
+  ];
 
   function getCardsPerSlide() {
     if (window.innerWidth >= 1200) return 5;
@@ -251,20 +53,23 @@ document.addEventListener("DOMContentLoaded", function () {
       slide.className = `carousel-item ${isActive}`;
       slide.innerHTML = `
           <div class="d-flex justify-content-center"> 
-              ${group.map((card) => `
-                  <a href="${card.url}" class="card" style="width: 18rem; margin-right: 15px; text-decoration: none; color: inherit;">
-                      <img src="${card.image}" class="card-img-top" alt="${card.title}">
+              ${group
+                .map(
+                  (card) => `
+                  <a href="${card.link}" class="card" style="width: 18rem; margin-right: 15px; text-decoration: none; color: inherit;">
+                      <img src="${card.image}" class="card-img-top">
                       <div class="card-body">
-                          <h5 class="card-title">${card.title}</h5>
-                          <p class="card-text">${card.text}</p>
+                          <h5>${card.name}</h5>
+                          <p>${card.subheading}</p>
                       </div>
                   </a>
-              `).join("")}
+              `
+                )
+                .join("")}
           </div>
       `;
       carouselInner.appendChild(slide);
     });
-    
   }
 
   function generateAllCarousels() {
