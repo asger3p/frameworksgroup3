9906c0cd2f77bb73ba2a16c3090c674c78ced1da
Stine Derror <stde@itu.dk>
2025-05-21 15:43:37 +0200

    removed get all baskets

--- Final_submission/Backend/router/basketRouter.js
+++ Final_submission/Backend/router/basketRouter.js
@@ -4,7 +4,6 @@ import * as basketController from '../controller/basketController.js';
 const router = Router();
 
 router.get('/:customerId', basketController.getBasket);
-router.get('/', basketController.getAllBaskets);
 router.put('/:customerId', basketController.updateBasket);
 router.delete('/:customerId/:productId', basketController.removeProductFromBasket);
 
